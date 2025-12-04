8c68e28e4397dbf840e5c60659df884cf6633a10
Ayoub <ay2oub49@gmail.com>
2025-12-04 12:21:47 +0100

    hello from dev

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello from Jenkins CI/CD!', version: '1.0.0' });
+  res.json({ message: 'Hello from Dev', version: '1.0.0' });
 });
 
 app.get('/health', (req, res) => {
